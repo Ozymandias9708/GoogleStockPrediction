091286b2d2558d2d91e1c376ba9c3c391a6485c3
Shubham Kumar <shubham9905374230@gmail.com>
2024-09-07 22:33:15 +0530

    fixed bugs

--- Recurrent_Neural_Networks.py
+++ Recurrent_Neural_Networks.py
@@ -1,66 +1,73 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout
 
-# Load and preprocess training data
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
-training_set = dataset_train.iloc[:, 1:2].values
+df            = dataset_train.copy()
+training_set  = df.iloc[:, 1:2].values
 
-sc = MinMaxScaler(feature_range=(0, 1))
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
 training_set_scaled = sc.fit_transform(training_set)
 
-X_train, y_train = [], []
-for i in range(60, len(training_set)):
+X_train = []
+y_train = []
+for i in range(60, 1258):
     X_train.append(training_set_scaled[i-60:i, 0])
     y_train.append(training_set_scaled[i, 0])
-
 X_train, y_train = np.array(X_train), np.array(y_train)
+
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
-# Build the LSTM model
-regressor = Sequential([
-    LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)),
-    Dropout(0.2),
-    LSTM(units=50, return_sequences=True),
-    Dropout(0.2),
-    LSTM(units=50, return_sequences=True),
-    Dropout(0.2),
-    LSTM(units=50),
-    Dropout(0.2),
-    Dense(units=1)
-])
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
 
-regressor.compile(optimizer='adam', loss='mean_squared_error')
-regressor.fit(X_train, y_train, epochs=100, batch_size=32)
+regressor = Sequential()
 
-# Load and preprocess test data
-dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
-real_stock_price = dataset_test.iloc[:, 1:2].values
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
 
-dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis=0)
-inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
-inputs = np.reshape(inputs, (-1, 1))
-inputs = sc.transform(inputs)
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences = False))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units = 1))
+
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+
+dataset_test     = pd.read_csv('Google_Stock_Price_Test.csv')
+df_test          = dataset_test.copy()
+real_stock_price = df_test.iloc[:, 1:2].values
+
+dataset_total = pd.concat((df['Open'], df_test['Open']), axis = 0)
+df_total      = dataset_total.copy()
+inputs        = df_total[len(df_total) - len(df_test) - 60 :].values
+inputs        = np.reshape(inputs, (-1, 1))
+inputs        = sc.transform(inputs)
 
 X_test = []
 for i in range(60, 80):
     X_test.append(inputs[i-60:i, 0])
-
 X_test = np.array(X_test)
+
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
-# Predict stock prices
 predicted_stock_price = regressor.predict(X_test)
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
-# Visualize the results
-plt.plot(real_stock_price, color='red', label='Real Google Stock Price')
-plt.plot(predicted_stock_price, color='blue', label='Predicted Google Stock Price')
+plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
 plt.title('Google Stock Price Prediction')
 plt.xlabel('Time')
-plt.ylabel('Stock Price')
+plt.ylabel('Google Price')
 plt.legend()
 plt.show()
